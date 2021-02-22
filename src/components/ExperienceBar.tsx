509a4343dd85b813c8574e94b064159c95fa07e9
Douglas Marcelino <33377432+TheeDouglasAM3@users.noreply.github.com>
2021-02-22 16:27:36 -0300

    Create ExperienceBar component

---
+++ src/components/ExperienceBar.tsx
@@ -0,0 +1,13 @@
+export function ExperienceBar() {
+  return (
+    <header className="experience-bar">
+      <span>0 xp</span>
+      <div>
+        <div style={{ width: '75%' }} />
+
+        <span className="current-experience" style={{ left: '75%' }}>450 xp</span>
+      </div>
+      <span>600 xp</span>
+    </header>
+  )
+}
