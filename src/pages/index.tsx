e1c1e35c60a55f6c531d1137689bede33ae3d28a
Douglas Marcelino <33377432+TheeDouglasAM3@users.noreply.github.com>
2021-02-23 19:05:55 -0300

    Update Home page

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,10 +1,30 @@
 import Head from 'next/head'
+import { CompletedChallenges } from '../components/CompletedChallenges';
+import { Countdown } from '../components/Countdown';
 import { ExperienceBar } from "../components/ExperienceBar";
+import { Profile } from '../components/Profile';
+
+import styles from '../styles/pages/Home.module.css'
 
 export default function Home() {
   return (
-    <div className="container">
+    <div className={styles.container}>
+
+      <Head>
+        <title>Início | move.it</title>
+      </Head>
+
       <ExperienceBar />
+      <section>
+        <div>
+          <Profile />
+          <CompletedChallenges />
+          <Countdown />
+        </div>
+        <div>
+
+        </div>
+      </section>
     </div>
   )
 }
