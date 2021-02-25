a6f7bcc96cd33a7f4f4849ce92bef2709c77e02e
Douglas Marcelino <33377432+TheeDouglasAM3@users.noreply.github.com>
2021-02-25 17:48:24 -0300

    Add ChallengesContext in Profile component

--- src/components/Profile.tsx
+++ src/components/Profile.tsx
@@ -1,6 +1,11 @@
+import { useContext } from 'react'
+import { ChallengesContext } from '../contexts/ChallengesContext'
 import styles from '../styles/components/Profile.module.css'
 
 export function Profile() {
+
+  const { level } = useContext(ChallengesContext)
+
   return (
     <div className={styles.profileContainer}>
       <img src="https://github.com/theedouglasam3.png" alt="Douglas Marcelino"/>
@@ -8,7 +13,7 @@ export function Profile() {
         <strong>Douglas Marcelino</strong>
         <p>
           <img src="icons/level.svg" alt="Level"/>
-          Level 1
+          Level {level}
         </p>
       </div>
     </div>
