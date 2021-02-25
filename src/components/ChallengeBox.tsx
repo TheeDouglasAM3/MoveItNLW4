c9d83d5cea000845c19d819a265436088c782d17
Douglas Marcelino <33377432+TheeDouglasAM3@users.noreply.github.com>
2021-02-25 17:48:05 -0300

    Update some components with CountdownContext

--- src/components/ChallengeBox.tsx
+++ src/components/ChallengeBox.tsx
@@ -1,10 +1,22 @@
 import { useContext } from 'react'
 import { ChallengesContext } from '../contexts/ChallengesContext'
+import { CountdownContext } from '../contexts/CountdownContext'
 import styles from '../styles/components/ChallengeBox.module.css'
 
 export function ChallengeBox() {
 
-  const { activeChallenge, resetChallenge } = useContext(ChallengesContext)
+  const { activeChallenge, resetChallenge, completeChallenge } = useContext(ChallengesContext)
+  const { resetCountdown } = useContext(CountdownContext)
+
+  function handleChallengeSucceeded(){
+    completeChallenge()
+    resetCountdown()
+  }
+
+  function handleChallengeFailed(){
+    resetChallenge()
+    resetCountdown()
+  }
 
   return (
     <div className={styles.challengeBoxContainer}>
@@ -24,7 +36,7 @@ export function ChallengeBox() {
               <button
                 type="button"
                 className={styles.challengeFailedButton}
-                onClick={resetChallenge}
+                onClick={handleChallengeFailed}
               >
                 Falhei
               </button>
@@ -32,6 +44,7 @@ export function ChallengeBox() {
               <button
                 type="button"
                 className={styles.challengeSucceededButton}
+                onClick={handleChallengeSucceeded}
               >
                 Completei
               </button>
